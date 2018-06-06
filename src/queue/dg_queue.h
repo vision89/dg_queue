98b728631b09872ce76cb06c8b7a1d1d6c712f45
Dustin Gulley <dlgulley411@gmail.com>
2018-06-05 22:19:25 -0400

    Changing filename

---
+++ src/queue/dg_queue.h
@@ -0,0 +1,8 @@
+/**
+ *
+ * @file dg_queue.h
+ * @brief this header file contains the dg_queue struct and all functions associated with the dg_queue.
+ * @author Dustin Gulley
+ * @date 06/05/2018
+ *
+ */
